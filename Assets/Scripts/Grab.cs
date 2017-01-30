918cbdad68d3d65a7a9030c531dc5a2b0a2b7edd
Eyal Shahar <eyal@shahar.co.il>
2017-01-30 00:58:09 -0800

    Initial git.

---
+++ Assets/Scripts/Grab.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grab : MonoBehaviour {
+
+	private bool isGrabbing = false;
+	private GameObject grabbedObject;
+	public LayerMask grabLayer;
+	public OVRInput.Controller controller;
+	public string[] buttons;
+	public float grabRadius = 0.1f;
+	public AudioClip audioClip;
+
+	// Update is called once per frame
+	void Update () {
+		bool buttonPressed = false;
+		for (int i=0; i<buttons.Length; i++) {
+			if(Input.GetAxis(buttons[i])==1) {
+				buttonPressed = true;
+				break;
+			}
+		}
+		if (!isGrabbing && buttonPressed) GrabObject();
+		if (isGrabbing && !buttonPressed) DropObject();
+
+	}
+
+	void GrabObject() {
+		RaycastHit[] hits;
+		hits = Physics.SphereCastAll(transform.position, grabRadius, transform.forward, 0f, grabLayer);
+		if (hits.Length < 1) return;
+		int closestObject = 0;
+		for(int i=0; i<hits.Length; i++) {
+			if (hits[i].distance < hits[closestObject].distance) closestObject = i;
+		}
+		grabbedObject = hits[closestObject].transform.gameObject;
+		grabbedObject.GetComponent<Rigidbody>().isKinematic = true;
+		grabbedObject.transform.position = transform.position;
+		grabbedObject.transform.parent = transform;
+
+		// HAPTIC FEEDBACK - NOT WORKING
+		OVRHapticsClip haptic = new OVRHapticsClip(audioClip);
+		OVRHaptics.RightChannel.Preempt(haptic);
+
+
+		isGrabbing = true;
+	}
+
+	void DropObject() {
+		if (grabbedObject == null) return;
+		grabbedObject.transform.parent = null;
+		grabbedObject.GetComponent<Rigidbody>().isKinematic = false;
+		grabbedObject.GetComponent<Rigidbody>().velocity = OVRInput.GetLocalControllerVelocity(controller);
+		grabbedObject = null;
+
+		isGrabbing = false;
+	}
+}
---
+++ Assets/Scripts/RestoreWhenFar.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RestoreWhenFar : MonoBehaviour {
+
+	private Vector3 originalPosition;
+	private Vector3 playerPosition;
+	public float maxDistance = 2f;
+
+	private void Start()
+	{
+		originalPosition = transform.position;
+		playerPosition = GameObject.Find("Player").transform.position;
+	}
+
+	void Update()
+	{
+		float distance = Vector3.Distance(transform.position, playerPosition);
+		if (distance>maxDistance) {
+			GetComponent<Rigidbody>().velocity = Vector3.zero;
+			GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+			transform.position = originalPosition;
+		}
+	}
+}
